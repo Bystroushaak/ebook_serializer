5d81a1a985cbef11ab7f7969b4964c45e6e559bc
Bystroushaak <bystrousak@kitakitsune.org>
2015-10-11 01:35:20 +0200

    Tests adjusted to new structure of the project.

--- tests/components/test_tools.py
+++ tests/components/test_tools.py
@@ -6,15 +6,15 @@
 # Imports =====================================================================
 import pytest
 
-from ebook_serializer import book
+from ebook_serializer.components.tools import to_absolute_url
+from ebook_serializer.components.tools import links_to_absolute_url
 
-from test_toc_guesser import toc_links
-from test_toc_guesser import read_data_context
+from ..test_toc_guesser import toc_links
+from ..test_toc_guesser import read_data_context
 
 
 # Variables ===================================================================
 BASE_URL = "http://pharo.gemtalksystems.com"
-
 
 
 # Fixtures ====================================================================
@@ -28,13 +28,13 @@ def test_to_absolute_url():
     absolute_url = "http://pharo.gemtalksystems.com/book/table-of-contents/"
     relative_url = "./book/table-of-contents/"  # notice the ./
 
-    assert absolute_url == book._to_absolute_url(
+    assert absolute_url == to_absolute_url(
         relative_url,
         base_url=BASE_URL
     )
 
 
 def test_links_to_absolute_url(toc_links, abs_toc_links):
-    abs_links = book.links_to_absolute_url(toc_links, base_url=BASE_URL)
+    abs_links = links_to_absolute_url(toc_links, base_url=BASE_URL)
 
     assert abs_links == abs_toc_links
